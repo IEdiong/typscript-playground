fb80b685347406c8f90e4b402dfeec9682107e1e
IEdiong@Mint <ediongi6@hotmail.com>
2021-09-03 00:53:40 +0100

    #0. first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// The Basics
+// The primitives: string, number, and boolean
+var fname = "Joe";
+var age = 23;
+var isSaved = true;
+// Arrays
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// The Basics
+// The primitives: string, number, and boolean
+let fname: string = "Joe";
+let age: number = 23;
+let isSaved: boolean = true;
+
+// Arrays
