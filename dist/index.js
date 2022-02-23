312319ab251b558d5fbc8c7c8fba94fb199e6866
iediong <ediongi6@hotmail.com>
2022-02-23 18:18:57 +0100

    learnt some more

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,44 @@
 "use strict";
-var fname = "Joe";
+var fname = 'Joe';
 var age = 23;
 var isSaved = true;
 var telNumber = [8012334444, 8112223222, 9012221212];
-var customer = ["Joe", "Jill", "Susan"];
-var userId = "justus24";
+var customer = ['Joe', 'Jill', 'Susan'];
+var userId = 'justus24';
 var fn2 = function (username) {
-    console.log("Hello " + username);
+    console.log("Hello ".concat(username));
 };
-var myName = "Ediongsenyene";
-var url = "https://www.g-lh.org/lh-02";
+var myName = 'Ediongsenyene';
+var url = 'https://www.g-lh.org/lh-02';
 var regexpTest = /\b^w+\b/g;
 console.log(url.match(regexpTest));
+var person = {
+    name: 'Joseph',
+    walk: function () {
+        console.log('Walking!!');
+    },
+    talk: function () {
+        console.log("".concat(this.name, " can talk."));
+    },
+    whoYou: function () {
+        console.log(this);
+    },
+};
+console.log(person.name);
+person.walk();
+var whoYou = person.whoYou;
+whoYou();
+var whoYouNew = person.whoYou.bind(person);
+whoYouNew();
+var square = function (n) { return n * n; };
+console.log(square(5));
+var jobs = [
+    { id: 1, isActive: true },
+    { id: 2, isActive: true },
+    { id: 3, isActive: false },
+];
+var activeJobs = jobs.filter(function (job) { return job.isActive; });
+console.log(activeJobs);
+var colors = ['red', 'green', 'blue'];
+var items = colors.map(function (color) { return "<li>".concat(color, "</li>"); });
+console.log(items);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 // The Basics
 
 // The primitives: string, number, and boolean
-let fname: string = "Joe";
+let fname: string = 'Joe';
 let age: number = 23;
 let isSaved: boolean = true;
 // The type names String, Number, and Boolean (starting with capital letters) are legal, but refer to some special built-in types that will very rarely appear in your code. Always use string, number, or boolean for types.
 
 // Arrays
 const telNumber: number[] = [8012334444, 8112223222, 9012221212];
-const customer: string[] = ["Joe", "Jill", "Susan"];
+const customer: string[] = ['Joe', 'Jill', 'Susan'];
 // You may also see this written as Array<number>, which means the same thing. We’ll learn more about the syntax T<U> when we cover generics.
 // Note that [number] is a different thing; refer to the section on Tuples.
 
 // any
-let userId: any = "justus24";
+let userId: any = 'justus24';
 // The any type is useful when you don’t want to write out a long type just to convince TypeScript that a particular line of code is okay.
 
 // noImplicitAny
@@ -33,9 +33,50 @@ const fn2 = (username: string) => {
 
 // Type Annotations on Variables
 // TypeScript doesn’t use “types on the left”-style declarations like int x = 0; Type annotations will always go after the thing being typed.
-let myName: string = "Ediongsenyene";
+let myName: string = 'Ediongsenyene';
 
 // Functions
-const url = "https://www.g-lh.org/lh-02";
+const url = 'https://www.g-lh.org/lh-02';
 const regexpTest = /\b^w+\b/g;
 console.log(url.match(regexpTest));
+
+const person = {
+  name: 'Joseph',
+  walk() {
+    console.log('Walking!!');
+  },
+  talk() {
+    console.log(`${this.name} can talk.`);
+  },
+  whoYou() {
+    console.log(this);
+  },
+};
+
+console.log(person.name);
+person.walk();
+
+const whoYou = person.whoYou;
+whoYou();
+const whoYouNew = person.whoYou.bind(person);
+whoYouNew();
+
+const square = (n: number) => n * n;
+console.log(square(5));
+
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false },
+];
+
+const activeJobs = jobs.filter((job) => job.isActive);
+console.log(activeJobs);
+// Arrow functions don't rebind the 'this' key
+// word.
+
+const colors = ['red', 'green', 'blue'];
+
+const items = colors.map((color) => `<li>${color}</li>`);
+
+console.log(items);
